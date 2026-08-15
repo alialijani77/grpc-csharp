dc6ddec97d38a90a770c1e32e36b1f1cb4c6495c
ali <alialijani1377@gmail.com>
2026-08-15 22:19:03 +0330

    Add grpc-csharp-server

---
+++ grpc-csharp-server/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using grpc_csharp_server.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace grpc_csharp_server.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ grpc-csharp-server/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace grpc_csharp_server.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
