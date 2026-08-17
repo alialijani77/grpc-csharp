6afa0e7cb35fda68f0d6be9b946c2caefd942640
ali <alialijani1377@gmail.com>
2026-08-17 21:19:50 +0330

    Add grpc console client for product service

--- ConsoleClient/Program.cs
+++ ConsoleClient/Program.cs
@@ -1,1 +1,40 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Grpc.Net.Client;
+using grpc_csharp_server.Protos;
+
+using var channel = GrpcChannel.ForAddress("https://localhost:7167");
+
+var client = new ProductService.ProductServiceClient(channel);
+
+// -----------------------------
+// GetProduct
+// -----------------------------
+
+var product = await client.GetProductAsync(
+    new GetProductRequest
+    {
+        Id = 1
+    });
+
+Console.WriteLine("Product:");
+Console.WriteLine($"Id: {product.Id}");
+Console.WriteLine($"Name: {product.Name}");
+Console.WriteLine($"Price: {product.Price}");
+
+
+// -----------------------------
+// GetProducts
+// -----------------------------
+
+var products = await client.GetProductsAsync(
+    new GetProductsRequest());
+
+Console.WriteLine();
+Console.WriteLine("All Products:");
+
+foreach (var item in products.Products)
+{
+    Console.WriteLine(
+        $"{item.Id} - {item.Name} - {item.Price}");
+}
+
+Console.ReadLine();
